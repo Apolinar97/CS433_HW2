1944a42c70cf15a2a8b3b54b379af6aeb6f56bf1
Apolinar <camac044@cougars.csusm.edu>
2019-02-26 17:13:35 -0700

    Added some conditions

--- sudokuValid.c
+++ sudokuValid.c
@@ -12,8 +12,10 @@ Due Date: 2/28/2019
 
 #define ROW 9
 #define COL 9
+#define SIZE_VALID_ARRAY 11
+
 //Sudoku Solution that will be used for testing puposed.
-int SudokuSolution[ROW][COL] = {
+int Sudoku[ROW][COL] = {
   {6,2,4,5,3,9,1,8,7},
   {5,1,9,7,2,8,6,3,4},
   {8,3,7,6,1,4,2,9,5},
@@ -24,15 +26,58 @@ int SudokuSolution[ROW][COL] = {
   {4,9,6,1,8,2,5,7,3},
   {2,8,5,4,7,3,9,1,6}
 };
+int validArray[SIZE_VALID_ARRAY] = {0,0,0,0,0,0,0,0,0,0,0};
+int validArray2[9] = {1,2,3,4,5,6,7,8,9};
+
 // row and column that will be checked, put into a struct.
 typedef struct {
-  int row;
-  int column;
-
+  int _row;
+  int _col;
 } parameters;
 
+void* rowValid(void* workingParam);
+void* colValid(void* workingParam);
 
 int main() {
   
   return 0;
 }
+
+void* rowValid(void* workingParam) { 
+  parameters *currentParam = workingParam;
+  int row = currentParam->_row;
+  int col = currentParam->_col;
+  int tempArray[9] = {1,2,3,4,5,6,7,8,9};
+  int success = 0; 
+  
+  for(int colum=0; colum<9; colum++)
+    {
+      for(int row_=0; row_<9; row_++) 
+	{
+       for(int i=0; i<9; i++)
+	 {   
+	   if(Sudoku[colum][row] == tempArray[i] && Sudoku[colum][row_] > 0 &&
+	      Sudoku[colum][row_] < 10)
+	     {
+	       success ++;
+	     }
+	   else
+	     {
+	       
+	     }
+	 }
+       if(success == 1)
+	 {
+	   int tempArray[9] = {1,2,3,4,5,6,7,8,9};
+	   sucess = 0;
+	 }
+	}
+    }
+}
+
+
+
+void* colValid(void* workingParam) {
+  ;
+}
+

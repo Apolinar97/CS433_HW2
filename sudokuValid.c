11b260de5f2d76bf2a3e33d204fda7358d12bc9b
Apolinar <camac044@cougars.csusm.edu>
2019-02-27 00:18:05 -0700

    Found bug cuasing seg. fault

--- sudokuValid.c
+++ sudokuValid.c
@@ -43,25 +43,125 @@ int main() {
   p0->_row = 0;
   p0->_col = 0;
 
+  parameters* _1st3x3 = (parameters*) malloc(sizeof(parameters));
+  _1st3x3->_row = 0;
+  _1st3x3->_col = 0;
+
+  parameters* _2nd3x3 = (parameters*) malloc(sizeof(parameters));
+  _2nd3x3->_row = 0;
+  _2nd3x3->_col = 3;
+
+  parameters* _3d3x3 = (parameters*) malloc(sizeof(parameters));
+  _3d3x3->_row=0;
+  _3d3x3->_col = 6;
+
+  parameters* _4th3x3 = (parameters*) malloc(sizeof(parameters));
+  _4th3x3->_row = 3;
+  _4th3x3->_col= 0;
+
+  parameters* _5th3x3 = (parameters*) malloc(sizeof(parameters));
+  _5th3x3->_row = 3;
+  _5th3x3->_col = 3;
+
+  parameters* _6th3x3 = (parameters*) malloc(sizeof(parameters));
+  _6th3x3->_row = 3;
+  _6th3x3->_col = 6;
+
+  parameters* _7th3x3 = (parameters*) malloc(sizeof(parameters));
+  _7th3x3->_row = 6;
+  _7th3x3->_col = 0;
+
+  parameters* _8th3x3 = (parameters*) malloc(sizeof(parameters));
+  _8th3x3->_row = 6;
+  _8th3x3->_col = 3;
+
+  parameters* _9th3x3 = (parameters*) malloc(sizeof(parameters));
+  _9th3x3->_row = 6;
+  _9th3x3->_col = 6;   
+  //end of parameters init.
+  
+
+
   //Threads section begin//
   pthread_t tRows;
   pthread_t tCols;
+  pthread_t unit1;
+  pthread_t unit2;
+  pthread_t unit3;
+  pthread_t unit4;
+  pthread_t unit5;
+  pthread_t unit6;
+  pthread_t unit7;
+  pthread_t unit8;
+  pthread_t unit9;
   
   //end threads section//
 
   //Return values
-  void* rows;
-  void* cols;
+  
+  void* return0;
+  void* return1;
+  void* return2;
+  void* return3;
+  void* return4;
+  void* return5;
+  void* return6;
+  void* return7;
+  void* return8;
+  void* return9;
+  void* return10;
+  void* return11;
+
   //end return values section // 
 
   //Initialize thread
   pthread_create(&tRows,NULL,rowValid,(void *) p0);
   pthread_create(&tCols,NULL,colValid,(void *) p0);
   
-
+  pthread_create(&unit1,NULL,valid3x3,(void *) _1st3x3);
+  pthread_create(&unit2,NULL,valid3x3,(void *) _2nd3x3);
+  pthread_create(&unit3,NULL,valid3x3,(void *) _3d3x3);   
+  pthread_create(&unit4,NULL,valid3x3,(void *) _4th3x3);
+  pthread_create(&unit5,NULL,valid3x3,(void *) _5th3x3);
+  pthread_create(&unit6,NULL,valid3x3,(void *) _6th3x3);
+  pthread_create(&unit7,NULL,valid3x3,(void *) _7th3x3);
+  pthread_create(&unit8,NULL,valid3x3,(void *) _8th3x3);
+  pthread_create(&unit9,NULL,valid3x3,(void *) _9th3x3);
+ 
   //Wait for threads//
-  pthread_join(tRows,&rows);
-  pthread_join(tCols,&cols);
+  pthread_join(tRows,&return0);
+  pthread_join(tCols,&return1);
+  pthread_join(unit1,&return2);
+  
+  pthread_join(unit2,&return3);
+  pthread_join(unit3,&return4);
+  
+ 
+  pthread_join(unit4,&return5);
+  pthread_join(unit5,&return6);
+  pthread_join(unit6,&return7);
+  pthread_join(unit7,&return8);
+  pthread_join(unit8,&return9);
+  pthread_join(unit9,&return10);
+  
+  /*
+  if( (long) return0 == 1 && (long) return1 == 1 && (long) return2 == 1 && (long) return3 == 1 &&
+      (long) return4 == 1 && (long) return5 == 1 && (long) return6 == 1 && (long) return7 == 1 &&
+      (long) return8 == 1 && (long) return9 == 1 && (long) return10 == 1)  {
+
+    printf("Valid!");
+  }
+  else
+    {
+      printf("Invalid Sudoku board");
+    }
+  */
+  //check if all returns have a value of 1.
+  if((long) return0==1 && (long) return1==1 && (long) return2==1 && (long)return3==1) {
+    printf("Valid!\n");
+  }
+  else
+    printf("invalid\n");
   
   return 0;
 }
@@ -72,7 +172,6 @@ void* rowValid(void* workingParam) {
   int startCol = data->_col;
   int tableValue;
   
-  printf("Initial values of row and col: %d:%d \n", startRow, startCol);
   
   for(int i = startRow; i<9; i++) {
     
@@ -85,19 +184,19 @@ void* rowValid(void* workingParam) {
 	
 	if(checker[tableValue] != 0)
 	  {
-	    printf("Invalid row.\n");
-	    return (void*) 0;
+	    // printf("Invalid row.\n");
+	    return (void*)  0;
 	  }
 	else {
 	  checker[tableValue] = 1;
-	  printf("Value of table.\n %d",tableValue);
+	  //printf("Value of table.\n %d",tableValue);
 	}
       }
     
   }
-  printf("Valid row!");
+  //printf("Valid row!");
   
-  return (void*) 1;
+  return (void*)  1;
   
 }//end of rowValid(...); 
 
@@ -113,11 +212,11 @@ void* colValid(void* workingParam) {
       tableValue = Sudoku[j][i];
 
       if(checker[tableValue] !=0) { //duplicate has apperead. 
-	return (void *) 0;
+	return (void*)  0;
       }
 
       else {
-	printf("tableValue: %d\n ", tableValue);
+	//printf("tableValue: %d\n ", tableValue);
 	checker[tableValue] = 1;
       }
       
@@ -134,26 +233,30 @@ void* valid3x3(void* workingParam) {
   parameters* data = (parameters*) workingParam;
   int startRow = data->_row;
   int startCol = data->_col;
-  int checker[9] = {0};
+  int checker[10] = {0};
   int tempNum;
-  for(int i = startRow; startRow<startRow+3; i++)
+  
+  for(int i = startRow; i<startRow+3; i++)
     {
-      for(int z = startCol; startCol=startCol+3; z++)
-	{
-	  tempNum = Sudoku[startRow][startCol];
+      for(int z = startCol; z<startCol+3; z++)
+	{	  
+	  tempNum = Sudoku[i][z];
+	  printf("tempNum: %d\n",tempNum);
 	  if(checker[tempNum] != 0)
 	    {
-	      return (void* ) 0;
+	      printf("Error!....\n");
+	      return (void*) 0;
 	    }
 	  else
 	    {
 	      checker[tempNum]=1;
+	      // printf("Okay! 3x3!\n");
 	    }
 	}//end innner for.
       
     }//end of outter for.
   
-  return (void*) 1;
+  return (void*)  1;
 }
 
   /*
